96dfda0464fdb6c0d8aa60ccb0d6b80e3ce73507
usama455 <muhammad.usama@surveyauto.com>
2021-08-02 23:38:33 +0500

    random twitter header generator

---
+++ config.js
@@ -0,0 +1,128 @@
+const Twitter = require("twitter");
+const fs = require("fs");
+const Promise = require("bluebird");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// auth methods
+const auth = () => {
+	let secret = {
+		consumer_key: process.env.API_KEY,
+		consumer_secret: process.env.SECRET_KEY,
+		access_token_key: process.env.ACCESS_TOKEN,
+		access_token_secret: process.env.ACCESS_TOKEN_SECRET
+	};
+
+	var client = new Twitter(secret);
+	return client;
+};
+
+// media upload methods
+
+const initMediaUpload = (client, pathToFile) => {
+	const mediaType = "image/jpg";
+	const mediaSize = fs.statSync(pathToFile).size;
+	return new Promise((resolve, reject) => {
+		client.post(
+			"media/upload",
+			{
+				command: "INIT",
+				total_bytes: mediaSize,
+				media_type: mediaType
+			},
+			(error, data, response) => {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					resolve(data.media_id_string);
+				}
+			}
+		);
+	});
+};
+
+const appendMedia = (client, mediaId, pathToFile) => {
+	const mediaData = fs.readFileSync(pathToFile);
+	return new Promise((resolve, reject) => {
+		client.post(
+			"media/upload",
+			{
+				command: "APPEND",
+				media_id: mediaId,
+				media: mediaData,
+				segment_index: 0
+			},
+			(error, data, response) => {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					resolve(mediaId);
+				}
+			}
+		);
+	});
+};
+
+const finalizeMediaUpload = (client, mediaId) => {
+	return new Promise((resolve, reject) => {
+		client.post(
+			"media/upload",
+			{
+				command: "FINALIZE",
+				media_id: mediaId
+			},
+			(error, data, response) => {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					resolve(mediaId);
+				}
+			}
+		);
+	});
+};
+
+const postReplyWithMedia = (client, mediaFilePath, replyTweet) => {
+	initMediaUpload(client, mediaFilePath)
+		.then((mediaId) => appendMedia(client, mediaId, mediaFilePath))
+		.then((mediaId) => finalizeMediaUpload(client, mediaId))
+		.then((mediaId) => {
+			console.log("Preparing Object");
+			let statusObj = {
+				status: "Hi @" + replyTweet.user.screen_name + ", here's your new header from usama",
+				in_reply_to_status_id: replyTweet.id_str,
+				media_ids: mediaId
+			};
+			client.post("statuses/update", statusObj, (error, tweetReply, response) => {
+				//if we get an error print it out
+				if (error) {
+					console.log("Error Replying ", error);
+				}
+
+				//print the text of the tweet we sent out
+				console.log(tweetReply.text);
+			});
+		});
+};
+
+const postReply = (client, message, replyTweet) => {
+	let statusObj = {
+		status: "Hi @" + replyTweet.user.screen_name + ", " + message,
+		in_reply_to_status_id: replyTweet.id_str
+	};
+
+	client.post("statuses/update", statusObj, (error, tweetReply, response) => {
+		//if we get an error print it out
+		if (error) {
+			console.log(error);
+		}
+
+		//print the text of the tweet we sent out
+		console.log(tweetReply.text);
+	});
+};
+
+module.exports = { auth, postReplyWithMedia, postReply };
---
+++ index.js
@@ -0,0 +1,42 @@
+const { auth, postReplyWithMedia, postReply } = require("./config.js");
+const fs = require('fs');
+const request = require('request');
+const client = auth();
+
+client.stream("statuses/filter", { track: "#randomheader" }, function (stream) {
+	console.log("Searching for tweets...");
+
+	// when a tweet is found
+	stream.on("data", function (tweet) {
+		console.log("Found one!");
+		console.log("Recieved tweet reading...", tweet.text);
+
+		// check if tweet contains some media
+		// if (tweet.text) {
+			console.log("Replying to tweet with image.");
+			downloadImage('https://picsum.photos/1500/500', tweet.user.screen_name, function(){
+  console.log('done');
+
+  postReplyWithMedia(client,`./media/${tweet.user.screen_name}.png`, tweet);
+});
+			
+		// } else {
+		// 	console.log("Tweet didn't provide media. Replying with message.");
+		// 	const message = "Oops, looks like you didn't provide a media file!";
+		// 	postReply(client, message, tweet);
+		// }
+
+		stream.on("error", function (error) {
+			console.log(error);
+		});
+	});
+});
+
+var downloadImage = function(uri, filename, callback){
+	request.head(uri, function(err, res, body){
+		if (err) console.log(err)
+		else if (res) console.log(res)
+	  request(uri).pipe(fs.createWriteStream(`./media/${filename}.png`)).on('close', callback);
+	});
+  };
+  
